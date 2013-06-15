5e0b7a5acaa110d75effc4116dc36df153b54557
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2013-06-15 16:14:35 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My4chan::Application.config.secret_token = '63139f6135bb7cca7967f6dec5c16a39e751c1771b2fdf3a707ff51367b86cf76224d0fe61bb253adc1e406d60e8d8f6874d97a467177bb30c3dfc6f5301a108'
